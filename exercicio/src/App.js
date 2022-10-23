9c3c916e2c8e9725c19bdec6dd1762b42798546b
LABNUISA <araujo_isaias@outlook.com>
2022-10-23 18:48:59 -0300

    concluido

--- exercicio/src/App.js
+++ exercicio/src/App.js
@@ -6,17 +6,49 @@ import Nav from "./Componentes/Nav/Nav";
 import "./Styles.css";
 
 export default function App() {
-  
+
+  const infVideo = [
+
+    {
+      titulo: "titulo do vídeo1",
+      image: <img src="https://picsum.photos/400/400?a=1" alt="" />
+
+    },
+
+    {
+      titulo: "titulo do vídeo2",
+      image: <img src="https://picsum.photos/400/400?a=1" alt="" />
+
+    },
+
+    {
+      titulo: "titulo do vídeo3",
+      image: <img src="https://picsum.photos/400/400?a=1" alt="" />
+
+    },
+    {
+      titulo: "titulo do vídeo4",
+      image: <img src="https://picsum.photos/400/400?a=1" alt="" />
+
+    },
+
+    
+  ]
+
+  function reproduzVideo() {
+    alert("O vídeo está sendo reproduzido");
+  }
   return (
     <div>
       <div className="tela-inteira">
         <Header />
 
         <main>
-          <Nav/>
+          <Nav />
 
-          <InfoUsuario/>
-          
+          <InfoUsuario informacoes={infVideo} reproduzir={reproduzVideo} />
+
+
 
         </main>
 
--- exercicio/src/Componentes/InfoUsuario/InfoUsuario.js
+++ exercicio/src/Componentes/InfoUsuario/InfoUsuario.js
@@ -2,22 +2,35 @@ import React from "react";
 
 const usuario = (props) => {
 
-    const titulo = "Título do vídeo";
 
-  function reproduzVideo() {
-    alert("O vídeo está sendo reproduzido");
-  }
+  return (
 
-    return (
+    <section className="painel-de-videos">
+      <div className="box-pagina-principal" onClick={props.reproduzir}>
+        {props.informacoes[0].image}
+        <h4>{props.informacoes[0].titulo}</h4>
+      </div>
 
-        <section className="painel-de-videos">
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-                <img src="https://picsum.photos/400/400?a=1 " alt="" />
-                <h4>{props.titulo}</h4>
-            </div>
+      <div className="box-pagina-principal" onClick={props.reproduzir}>
+        {props.informacoes[1].image}
+        <h4>{props.informacoes[1].titulo}</h4>
+      </div>
 
-        </section>
-    )
+      <div className="box-pagina-principal" onClick={props.reproduzir}>
+        {props.informacoes[2].image}
+        <h4>{props.informacoes[2].titulo}</h4>
+      </div>
+
+      <div className="box-pagina-principal" onClick={props.reproduzir}>
+        {props.informacoes[3].image}
+        <h4>{props.informacoes[3].titulo}</h4>
+      </div>
+
+
+
+    </section>
+  )
 }
+
 
 export default usuario
